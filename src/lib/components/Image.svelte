75dcb7787ea275cdca93c22f79793e0ef2cbcf38
bluepnwage <a.carty2555@gmail.com>
2023-02-05 13:57:47 -0400

    turn progress into component

--- src/lib/components/Image.svelte
+++ src/lib/components/Image.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import type { DownloadData, ImageFormats } from "$lib/download-image";
   import { downloadImage } from "$lib/download-image";
+  import Progress from "./Progress.svelte";
   export let image: DownloadData;
   export let format: ImageFormats;
   export let publicId: string;
@@ -51,45 +52,5 @@
   <div class="flex justify-end pr-3">
     <p class="text-gray-600">{progress}%</p>
   </div>
-  <div class="progress mb-4 bg-gray-300 border border-gray-400">
-    <div
-      role="progressbar"
-      aria-labelledby={imageLabel}
-      aria-valuenow={progress}
-      data-loading={loading}
-      style="width: {loading ? '' : `${progress}%`}"
-      class="progress-bar  progress-loading bg-indigo-600"
-    />
-  </div>
+  <Progress {progress} {loading} {imageLabel} />
 </div>
-
-<style>
-  .progress {
-    height: 4px;
-    width: 100%;
-    position: relative;
-    overflow: hidden;
-  }
-  .progress-bar {
-    position: absolute;
-    height: 100%;
-    width: 100%;
-    transition: all 200ms ease;
-  }
-
-  .progress-bar[data-loading="true"] {
-    animation: animate 2s cubic-bezier(0.785, 0.135, 0.15, 0.86) infinite;
-  }
-
-  @keyframes animate {
-    0% {
-      transform: translateX(-100%) scaleX(100%);
-    }
-    50% {
-      transform: translateX(0%) scaleX(25%);
-    }
-    100% {
-      transform: translateX(100%) scaleX(100%);
-    }
-  }
-</style>
---
+++ src/lib/components/Progress.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  export let progress:number = 0;
+  export let imageLabel: string
+  export let loading:boolean
+</script>
+
+<div class="progress mb-4 bg-gray-300 border border-gray-400">
+  <div
+    role="progressbar"
+    aria-labelledby={imageLabel}
+    aria-valuenow={progress}
+    data-loading={loading}
+    style="width: {loading ? '' : `${progress}%`}"
+    class="progress-bar  progress-loading bg-indigo-600"
+  />
+</div>
+
+<style>
+  .progress {
+    height: 4px;
+    width: 100%;
+    position: relative;
+    overflow: hidden;
+  }
+  .progress-bar {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    transition: all 200ms ease;
+  }
+
+  .progress-bar[data-loading="true"] {
+    animation: animate 2s cubic-bezier(0.785, 0.135, 0.15, 0.86) infinite;
+  }
+
+  @keyframes animate {
+    0% {
+      transform: translateX(-100%) scaleX(100%);
+    }
+    50% {
+      transform: translateX(0%) scaleX(25%);
+    }
+    100% {
+      transform: translateX(100%) scaleX(100%);
+    }
+  }
+</style>
